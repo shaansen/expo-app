627cde0c9ad8da788307eb4371bd8fee44768c64
Shantanu Sengupta <shantanu.sengupta2008@gmail.com>
2018-11-02 20:28:12 -0400

    Square box in the middle of the screen

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { AppRegistry, View, StyleSheet } from 'react-native'
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <View style={styles.box} />
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
-});
+  box: {
+    width: 200,
+    height: 200,
+    backgroundColor: 'skyblue',
+    borderWidth: 2,
+    borderColor: 'steelblue',
+    borderRadius: 20,
+  },
+})
+
+AppRegistry.registerComponent('App', () => App)
